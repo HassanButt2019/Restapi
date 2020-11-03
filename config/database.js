4bdfd482e6bc34771ba4ebab2385d98d6c490c57
Hassan Butt <hassanqa13@gmail.com>
2020-11-03 14:35:05 +0500

    make it better2

--- config/database.js
+++ config/database.js
@@ -3,9 +3,6 @@ const mongoose = require('mongoose');
 
 const connectDb = async () =>{
 
-
-  try {
-    
   
   const conn = await mongoose.connect('mongodb+srv://yah22:honda123@societymanagement.l3x9d.mongodb.net/societymanagement?retryWrites=true&w=majority',{
   useNewUrlParser: true,
@@ -14,15 +11,6 @@ const connectDb = async () =>{
   useUnifiedTopology:true
   });
   console.log(`Mongo Db Connected: ${conn.connection.host}`);
-} catch (error) {
-    console.log(error);
-    process.exit(1);
 }
-
-
-}
-
-
-
 
 module.exports = connectDb;
