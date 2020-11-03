5394aa2c883f0191a30a57c3b4576352de4e25f4
Hassan Butt <hassanqa13@gmail.com>
2020-11-03 14:50:05 +0500

    make it better5

--- config/database.js
+++ config/database.js
@@ -6,7 +6,7 @@ const connectDb = async () =>{
   
   const conn = await mongoose.connect('mongodb+srv://yah22:honda123@societymanagement.l3x9d.mongodb.net/societymanagement?retryWrites=true&w=majority',{
   useNewUrlParser: true,
-  useCreateIndex:true,
+ // useCreateIndex:true,
   useFindAndModify:false,
   useUnifiedTopology:true
   });
