0ceee46b2a284752ec6e651f72bdc4d85e0299b6
Hassan Butt <hassanqa13@gmail.com>
2020-11-03 15:50:05 +0500

    make it better8

--- config/database.js
+++ config/database.js
@@ -6,7 +6,7 @@ const connectDb = async () =>{
   
   const conn = await mongoose.connect('mongodb://yah22:honda123@societymanagement-shard-00-00.l3x9d.mongodb.net:27017,societymanagement-shard-00-01.l3x9d.mongodb.net:27017,societymanagement-shard-00-02.l3x9d.mongodb.net:27017/societymanagement?ssl=true&replicaSet=atlas-pt9ekw-shard-0&authSource=admin&retryWrites=true&w=majority',{
   useNewUrlParser: true,
- // useCreateIndex:true,
+  useCreateIndex:true,
   useFindAndModify:false,
   useUnifiedTopology:true
   });
