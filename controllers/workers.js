14407302f34e486e17e78bfa32a34ab6b83b3d85
YassaFareed <yassa.fareed99@gmail.com>
2020-12-13 00:41:54 +0500

    Update workers.js

--- controllers/workers.js
+++ controllers/workers.js
@@ -4,7 +4,7 @@ const User = require('../models/user');
 //@route post /api/v1/workers
 exports.getWorkers = async(req , resp , next)=>{
    const {type} = req.body;
-
+  console.log(type);
 
 
   var workers = await User.find({"type":type});
@@ -20,4 +20,49 @@ exports.getWorkers = async(req , resp , next)=>{
 resp
   .status(200)
   .json({ success: true, data:workers });
+}
+
+
+//@desc delete by workers
+//@route delete /api/v1/workers
+exports.deleteWorkerByNameAndType = async( req, res, next) => {
+  try {
+    const {name} = req.body;
+     const {type} = req.body;
+   // console.log(name,type);
+  
+    const user = await User.findOneAndDelete({ "name" : name, "type": type });
+  
+    if(!user){
+      return res.status(400),json({success: false});
+    }
+
+    res.status(200).json({success: true, data:{} });
+  } catch (err) {
+    res.status(400).json({success: false});
+    
+  }
+}
+
+
+
+//@desc update worker details by id
+//@route put /api/v1/workers/:id
+exports.updateWorkerDetail = async( req, res, next) => {  //note: there is no check that if a person is worker or not
+  try {
+   
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true, //to get updated or new data
+      runValidators: true, //to run validator on data as well
+  });
+  
+    if(!user){
+      return res.status(400),json({success: false});
+    }
+
+    res.status(200).json({success: true, data: user });
+  } catch (err) {
+    res.status(400).json({success: false});
+    
+  }
 }
