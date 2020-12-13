723242f245f797fb774f4d01d4434016a96d3881
YassaFareed <yassa.fareed99@gmail.com>
2020-12-14 01:02:02 +0500

    Update workerstatus.js

--- routes/workerstatus.js
+++ routes/workerstatus.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {getworkerStatus,updateWorkerHours,updateWorkerDay,updateWorkerDate} = require('../controllers/workerstatus');
+const {getworkerStatus,updateWorkerHours,updateWorkerDay,updateWorkerDate,updateCheckinStatus,updateCheckoutStatus} = require('../controllers/workerstatus');
 const router = express.Router();
 
 
@@ -16,5 +16,11 @@ router.route('/day')
 
 router.route('/date')
     .post(updateWorkerDate);
+
+router.route('/checkin')
+    .post(updateCheckinStatus);
+
+router.route('/checkout')
+    .post(updateCheckoutStatus);
 
 module.exports = router;
