114e6c9b9789521976e3d742e2943750e288179c
Hassan Butt <hassanqa13@gmail.com>
2020-12-18 16:34:10 +0500

    changed model of complains

--- models/complains.js
+++ models/complains.js
@@ -1,32 +1,34 @@
 const mongoose = require('mongoose');
 
 const complainSchema = new mongoose.Schema({
-    title:{
-        type:String,
-        required:true,
-        unique:true
-    },
-    name:{
-        type:String,
-    },
-    building:{
-        type:String,
-    },
-    flat:{
-        type:Number,
-    },
-    createdAt:{
-        type:Date,
-        default:Date.now
-    },
-    status:{
-        type:String,
-        enum:['Not Listened', 'In Progress', 'Listened'],
-    },
-    description:{
-        type:String,
-        required:[true,'Please add your complain'],
-    }
+        title:{
+            type:String,
+        },
+        email:{
+            type:String,
+         },
+
+        name:{
+            type:String,
+        },
+        building:{
+            type:String,
+        },
+        flat:{
+            type:Number,
+        },
+        createdAt:{
+            type:Date,
+            default:Date.now
+        },
+        status:{
+            type:String,
+            enum:['Not Listened', 'In Progress', 'Listened'],
+        },
+        description:{
+            type:String,
+            required:[true,'Please add your complain'],
+        }
 });
 
 
